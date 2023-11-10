ae1d8f3a9c9bda42c6803b2e964aa5c877d6aa80
vishalmet <vishalmet003@gmail.com>
2023-11-10 11:23:28 +0530

    res

--- portfolio/src/components/securedapp.jsx
+++ portfolio/src/components/securedapp.jsx
@@ -34,16 +34,16 @@ const SecureDApp = () => {
             <div className=" m-6 p-10 rounded-3xl md:m-10 lg:m-16"style={combinedStyles}>
                 <div className=" pl-1 space-y-1 md:pl-6 lg:pl-10" >
                     <p className='  text-xl bg-gradient-to-t from-pink-500 via-red-500 to-yellow-500 bg-clip-text text-transparent pr-3 pb-3 orbitron-font font-bold tracking-wide md:text-3xl' >## Intern at SecureDApp</p>
-                    <p className=' text-xl  orbitron-font md:text-2xl'>**Frontend Developer & Growth Hacks** </p>
-                    <p className=' text-xl  orbitron-font md:text-2xl '>*<span className=' font-semibold'>Duration:</span> July 10, 2023 - September 24, 2023*</p>
+                    <p className=' text-xl  orbitron-font md:text-2xl text-white'>**Frontend Developer & Growth Hacks** </p>
+                    <p className=' text-xl  orbitron-font md:text-2xl text-white '>*<span className=' font-semibold'>Duration:</span> July 10, 2023 - September 24, 2023*</p>
                 </div>
                 <div className="pl-1 pt-10 space-y-1 md:pl-6 lg:pl-10">
                     <p className='text-xl bg-gradient-to-t from-pink-500 via-red-500 to-yellow-500 bg-clip-text text-transparent pr-3 pb-3 orbitron-font font-bold tracking-wide md:text-3xl'>### Overview</p>
-                    <p className=' text-xl kaushan-font tracking-wide md:text-2xl'>I had the opportunity to intern at SecureDApp, where I worked as a Frontend Developer while exploring and implementing various growth hacking strategies. During my internship, I gained valuable experience in web development, user interface design, and marketing techniques.</p>
+                    <p className=' text-xl kaushan-font tracking-wide text-white md:text-2xl'>I had the opportunity to intern at SecureDApp, where I worked as a Frontend Developer while exploring and implementing various growth hacking strategies. During my internship, I gained valuable experience in web development, user interface design, and marketing techniques.</p>
                 </div>
                 <div className="pl-1 pt-10 space-y-1 md:pl-6 lg:pl-10">
                     <p className='text-xl bg-gradient-to-t from-pink-500 via-red-500 to-yellow-500 bg-clip-text text-transparent pr-3 pb-3 orbitron-font font-bold tracking-wide md:text-3xl'>### Key Achievements and Responsibilities</p>
-                    <p className=' text-xl kaushan-font tracking-wide md:text-2xl'>
+                    <p className=' text-xl kaushan-font tracking-wide text-white md:text-2xl'>
                         <h1><span className='text-red-400'>-</span> Collaborated with the development team to create responsive and user-friendly web applications.</h1>
                         <h1><span className='text-red-400'>-</span>Participated in brainstorming sessions with the development team and to devise growth hacking strategies for user acquisition and retention.</h1>
                         <h1><span className='text-red-400'>-</span>Contributed to content marketing efforts, including blog posts in various social media like Discord, Twitter to increase brand awareness.</h1>
@@ -51,7 +51,7 @@ const SecureDApp = () => {
                 </div>
                 <div className="pl-1 pt-10 space-y-1 md:pl-6 lg:pl-10">
                     <p className='text-xl bg-gradient-to-t from-pink-500 via-red-500 to-yellow-500 bg-clip-text text-transparent pr-3 pb-3 orbitron-font font-bold tracking-wide md:text-3xl'>### Technologies and Tools Used</p>
-                    <p className=' text-xl kaushan-font tracking-wide md:text-2xl'>
+                    <p className=' text-xl kaushan-font tracking-wide text-white md:text-2xl'>
                         <h1><span className='text-red-400'>-</span>HTML, CSS, JavaScript</h1>
                         <h1><span className='text-red-400'>-</span>React.js</h1>
                         <h1><span className='text-red-400'>-</span>UI/UX Design</h1>
@@ -60,7 +60,7 @@ const SecureDApp = () => {
                 </div>
                 <div className="pl-1 pt-10 space-y-1 md:pl-6 lg:pl-10">
                     <p className='text-xl bg-gradient-to-t from-pink-500 via-red-500 to-yellow-500 bg-clip-text text-transparent pr-3 pb-3 orbitron-font font-bold tracking-wide md:text-3xl'>### Lessons Learned</p>
-                    <p className=' text-xl kaushan-font tracking-wide md:text-2xl'>My internship at SecureDApp allowed me to develop both technical and soft skills. I enhanced my proficiency in frontend development, gained experience in growth hacking, and improved my ability to collaborate in a dynamic team environment. I also learned the importance of data-driven decision-making and the value of user-centric design.</p>
+                    <p className=' text-xl text-white kaushan-font tracking-wide md:text-2xl'>My internship at SecureDApp allowed me to develop both technical and soft skills. I enhanced my proficiency in frontend development, gained experience in growth hacking, and improved my ability to collaborate in a dynamic team environment. I also learned the importance of data-driven decision-making and the value of user-centric design.</p>
                 </div>
             </div>
             <p className='text-white text-shadow-xs text-2xl flex justify-center pt-6 pb-6 kaushan-font md:text-3xl'>Made with - <span className='text-white'>❤️</span>- by Vishal Aakash</p>
